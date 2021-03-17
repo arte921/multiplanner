b3f9a03958f55ce6ca52f5f7e6e6c0d5496e5d68
Ard-Jan van Etten <ardjanve@gmail.com>
2021-03-17 09:43:21 +0100

    afstand werkend

--- functies/stationAfstand.js
+++ functies/stationAfstand.js
@@ -1,27 +1,11 @@
 const readJSONSync = require('./readJSONSync.js');
 const coordinaatAfstand = require('./coordinaatAfstand.js');
-const spoorkaart = readJSONSync("spoorkaart").payload.features;
-const config = readJSONSync("config");
+const afstanden = readJSONSync("afstanden");
 
 module.exports = (station1, station2) => {
     const station1KleineLetters = station1.toLowerCase();
     const station2KleineLetters = station2.toLowerCase();
 
-    const feature = spoorkaart.find((feature) => {
-        return feature.properties.from == station1KleineLetters && feature.properties.to == station2KleineLetters ||
-        feature.properties.from == station2KleineLetters && feature.properties.to == station1KleineLetters
-    });
-
-    if (!feature) return 0;
-
-    const featureId = feature.properties.from + feature.properties.to;
-
-    let afstand = 0;
-    feature.geometry.coordinates.forEach((coordinaat, index) => {
-        if (index > 1) afstand += coordinaatAfstand(coordinaat, feature.geometry.coordinates[index - 1])
-    });
-
-    if (afstand == 0) console.log(station1KleineLetters, station2KleineLetters);
-
-    return afstand;
+    const feature = afstanden.find((feature) => feature.van == station1KleineLetters && feature.naar == station2KleineLetters || feature.van == station2KleineLetters && feature.naar == station1KleineLetters);
+    return feature.afstand;
 };
--- update.js
+++ update.js
@@ -1,9 +1,21 @@
 const writeJSON = require("./functies/writeJSON.js");
 const haalDataOp = require('./functies/haalDataOp.js');
 const writeTXT = require('./functies/writeTXT.js');
+const coordinaatAfstand = require('./functies/coordinaatAfstand.js');
+const polylineAfstand = (polyline) => {
+    let afstand = 0;
+    for (let i = 1; i < polyline.length; i++) {
+        afstand += coordinaatAfstand(polyline[i], polyline[i - 1]);
+    }
+    return afstand;
+};
 
 (async () => {
-    writeJSON(await haalDataOp('/Spoorkaart-API/api/v1/spoorkaart/'), 'spoorkaart');
+    writeJSON((await haalDataOp('/Spoorkaart-API/api/v1/spoorkaart/')).payload.features.map((feature) => ({
+        van: feature.properties.from,
+        naar: feature.properties.to,
+        afstand: polylineAfstand(feature.geometry.coordinates)
+    })), 'afstanden');
     const stations = await haalDataOp('/reisinformatie-api/api/v2/stations');
     const geformatterdestations = stations.payload.filter((station) => station.land == "NL").map((station) => ({
             code: station.code.toLowerCase(),
