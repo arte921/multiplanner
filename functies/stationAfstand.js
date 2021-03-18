adebbc955da7215b6ac48202a0669be4d8826944
Ard-Jan van Etten <ardjanve@gmail.com>
2021-03-18 09:51:38 +0100

    feature niet gevonden fail-silent

--- functies/stationAfstand.js
+++ functies/stationAfstand.js
@@ -1,5 +1,4 @@
 const readJSONSync = require('./readJSONSync.js');
-const coordinaatAfstand = require('./coordinaatAfstand.js');
 const afstanden = readJSONSync("afstanden");
 
 module.exports = (station1, station2) => {
@@ -7,5 +6,6 @@ module.exports = (station1, station2) => {
     const station2KleineLetters = station2.toLowerCase();
 
     const feature = afstanden.find((feature) => feature.van == station1KleineLetters && feature.naar == station2KleineLetters || feature.van == station2KleineLetters && feature.naar == station1KleineLetters);
+    if (!feature) return 0;
     return feature.afstand;
 };
