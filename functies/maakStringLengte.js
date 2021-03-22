fb090f209b24a3db749cb8ac9c79e85d8aaceb89
Ard-Jan van Etten <ardjanve@gmail.com>
2021-03-22 17:44:47 +0100

    minimaalNul

--- functies/maakStringLengte.js
+++ functies/maakStringLengte.js
@@ -1,1 +1,3 @@
-module.exports = (string, lengte, cijfers = `${string}`.length) => `${" ".repeat(cijfers - `${string}`.length)}${string}${" ".repeat(lengte - cijfers)}`;
+const minimaalNul = (getal) => getal < 0 ? 0 : getal; 
+
+module.exports = (string, lengte, cijfers = `${string}`.length) => `${" ".repeat(minimaalNul(cijfers - `${string}`.length))}${string}${" ".repeat(lengte - cijfers)}`;
