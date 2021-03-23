f8a97bfcfbc836cd72e31b739425b6994e524407
Ard-Jan van Etten <ardjanve@gmail.com>
2021-03-23 17:22:09 +0100

    leading \n

--- functies/writeTXT.js
+++ functies/writeTXT.js
@@ -1,4 +1,4 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = async (input, locatie) => await fs.promises.writeFile(path.join("opslag", locatie + ".txt"), Array.isArray(input) ? input.join("\n") + "\n" : input);
+module.exports = async (input, locatie) => await fs.promises.writeFile(path.join("opslag", locatie + ".txt"), (Array.isArray(input) ? input.join("\n") : input) + "\n");
