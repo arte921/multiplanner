9e11302d3ca765800a784a609dd3aabd67dfd73c
Ard-Jan van Etten <ardjanve@gmail.com>
2021-03-23 08:31:41 +0100

    maaktabel abominatie

--- functies/formatteerReis.js
+++ functies/formatteerReis.js
@@ -10,14 +10,23 @@ const vertaalZijde = (zijde) => ({
     RIGHT: "rechterzijde"
 })[zijde];
 
-const wachtlengte = 3;
-const wachtcijfers = 2;
-const infolengte = 21;
+const maakTabel = (data) => data.map((rij) => rij.map((waarde, kolomIndex, _, breedte = data.map((rij) => `${rij[kolomIndex]}`.length).reduce((hoogste, huidige) => Math.max(huidige, hoogste))) => maakStringLengte(waarde, breedte, isNaN(waarde) ? undefined : breedte)).join(" ")).join("\n");
 
 module.exports = (reis) => {
-    resultaatString = reis.reis.map((rit, index) => {
+    const infoTabel = maakTabel([
+        ["Prijs", `€${reis.prijs / 100}`],
+        ["Wachttijd", formateerTijdsduurMinuten(reis.stationstijd)],
+        ["Rijtijd", formateerTijdsduurMinuten(reis.treintijd)],
+        ["Totale reistijd", formateerTijdsduurMinuten(reis.reistijd)],
+        ["Hemelsbrede afstand", `${Math.round(reis.hemelsbredeafstand)} kilometer`],
+        ["Afgelegde afstand", `${Math.round(reis.afstand)} kilometer`]
+    ]);
+
+    const resultaatString = maakTabel([[["Tijd", "Na", "Actie"]].concat(...reis.reis.map((rit) => {
         const zijdeDeel = rit.uitstapzijde ? ` aan de ${vertaalZijde(rit.uitstapzijde)}` : '';
-        return `${formatteerDatum(rit.vertrektijd)} ${maakStringLengte(rit.overstaptijd, wachtlengte, wachtcijfers)}Vertrek in de ${rit.categorie} richting ${rit.richting} op spoor ${rit.vertrekspoor}.\n${formatteerDatum(rit.aankomsttijd)} ${maakStringLengte(rit.ritduur, wachtlengte, wachtcijfers)}Stap${zijdeDeel} uit in ${rit.aankomststationnaam}.`;
-    }).join('\n');
-    return `${maakStringLengte("Prijs", infolengte)}€${reis.prijs / 100}\n${maakStringLengte("Wachttijd", infolengte)}${formateerTijdsduurMinuten(reis.stationstijd)}\n${maakStringLengte("Rijtijd", infolengte)}${formateerTijdsduurMinuten(reis.treintijd)}\n${maakStringLengte("Totale reistijd", infolengte)}${formateerTijdsduurMinuten(reis.reistijd)}\n${maakStringLengte("Hemelsbrede afstand", infolengte)}${Math.round(reis.hemelsbredeafstand)} kilometer\n${maakStringLengte("Afgelegde afstand", infolengte)}${Math.round(reis.afstand)} kilometer\nTijd  Na Actie\n${resultaatString}`;
+        return [[formatteerDatum(rit.vertrektijd), rit.overstaptijd, `Vertrek in de ${rit.categorie} richting ${rit.richting} op spoor ${rit.vertrekspoor}.`],
+            [formatteerDatum(rit.aankomsttijd), rit.ritduur, `Stap${zijdeDeel} uit in ${rit.aankomststationnaam}.`]];
+    }))][0]);
+
+    return `${infoTabel}\n${resultaatString}`;
 };
