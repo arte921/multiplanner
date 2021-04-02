70a060caaf34299726dedf31885dee05bd1f0941
Ard-Jan van Etten <ardjanve@gmail.com>
2021-04-02 21:57:45 +0200

    reverse bug lmao

--- functies/haalPolylineOp.js
+++ functies/haalPolylineOp.js
@@ -5,6 +5,6 @@ module.exports = (stationa, stationb) => {
     const rechtefeature = spoorkaart.payload.features.find((feature) => feature.properties.from == stationa.code && feature.properties.to == stationb.code);
     if (rechtefeature) return rechtefeature.geometry.coordinates;
     const omgekeerdefeature = spoorkaart.payload.features.find((feature) => feature.properties.to == stationa.code && feature.properties.from == stationb.code);
-    if (omgekeerdefeature) return omgekeerdefeature.geometry.coordinates.reverse();
-    return [stationb.coordinaat, stationa.coordinaat];
+    if (omgekeerdefeature) return [...omgekeerdefeature.geometry.coordinates].reverse();
+    return [stationa.coordinaat, stationb.coordinaat];
 };
