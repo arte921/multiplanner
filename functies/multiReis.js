d0bf9e6fbeeaffbbd44f4c47ec507084e9b70fce
Ard-Jan van Etten <ardjanve@gmail.com>
2021-04-02 14:52:21 +0200

    cleanup

--- functies/multiReis.js
+++ functies/multiReis.js
@@ -9,17 +9,11 @@ const {
 
 module.exports = async (route, startmoment, begintijd) => {
     let volgRitNummer;
-    let volgendeDatum = startmoment;
-    
-    let beginDatum = begintijd;
-    volgendeDatum = new Date(volgendeDatum.getTime() - 2 * 60 * 1000);
+    let volgendeDatum = new Date(startmoment.getTime() - 2 * 60 * 1000);
 
     let resultaat = [];
     let totalePrijsCent = 0;
     let urls = [];
-
-    let begincoordinaat;
-    let eindcoordinaat;
 
     let treintijd = 0;
     let stationstijd = 0;
@@ -27,19 +21,16 @@ module.exports = async (route, startmoment, begintijd) => {
     for (let i = 1; i < route.length; i++) {
         const trip = await vroegsteVolledigeReis(route[i - 1], route[i], volgendeDatum, volgRitNummer);
         urls.push(trip.shareUrl.uri);
-        // await require('./writeJSON.js')(trip, 'bs');
-        // return;
+        
         const rit = trip.legs.map(extractLeg);
         totalePrijsCent += trip.productFare.priceInCentsExcludingSupplement; //priceInCents;
-
-        if (i == 1) begincoordinaat = [trip.legs[0].origin.lng, trip.legs[0].origin.lat];
-        if (i == route.length - 1) eindcoordinaat = [trip.legs[trip.legs.length - 1].destination.lng, trip.legs[trip.legs.length - 1].destination.lat];
 
         volgendeDatum = aankomstTijd(trip);
         volgRitNummer = rit[rit.length - 1].ritnummer;
         resultaat.push(...rit);
     }
 
+    let beginDatum = begintijd;
     for (const [index, rit] of resultaat.entries()) {
         rit.overstaptijd = Math.floor((rit.vertrektijd - beginDatum) / 60 / 1000);
         if (index > 0) stationstijd += rit.overstaptijd;
@@ -51,7 +42,6 @@ module.exports = async (route, startmoment, begintijd) => {
     let gepaseerdeStations = [];
     resultaat.forEach((reisdeel, reisdeelIndex) => reisdeel.stations.filter((_, stationIndex) => reisdeelIndex == 0 || stationIndex > 0).forEach((station) => gepaseerdeStations.push(station)));
     const polyline = stationsLijstPolyline(gepaseerdeStations);
-
 
     return {
         prijs: totalePrijsCent,
