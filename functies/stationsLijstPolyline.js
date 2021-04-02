271fcf019d2ff2b82b70d21f18d6bfa4a3320e4c
Ard-Jan van Etten <ardjanve@gmail.com>
2021-04-02 11:52:10 +0200

    kaart polyline fix

--- functies/stationsLijstPolyline.js
+++ functies/stationsLijstPolyline.js
@@ -1,7 +1,5 @@
-const readJSONSync = require('./readJSONSync.js');
 const vindStation = require('./vindStation.js');
-
-const spoorkaart = readJSONSync("spoorkaart");
+const haalPolylineOp = require("./haalPolylineOp.js");
 
 module.exports = (stations) => {
     let vorigStation = "";
@@ -11,12 +9,7 @@ module.exports = (stations) => {
         const volledigStation = vindStation(station);
         
         if (index != 0) {
-            const feature = spoorkaart.payload.features.find((feature) => feature.properties.to == volledigStation.code && feature.properties.from == vorigStation.code || feature.properties.from == vorigStation.code && feature.properties.to ==  volledigStation.code);
-            const coordinaten = feature ? feature.geometry.coordinates : [vorigStation.coordinaat, volledigStation.coordinaat]; 
-
-            // console.log(feature);
-
-            polyline.push(...coordinaten.map((coordinaat) => ({
+            polyline.push(...haalPolylineOp(volledigStation, vorigStation).map((coordinaat) => ({
                 lat: coordinaat[1],
                 lng: coordinaat[0]
             })));
