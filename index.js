23a1100db2292780d448216012fe2736f94c3b48
Ard-Jan van Etten <ardjanve@gmail.com>
2021-04-05 08:31:07 +0200

    simpeler cli gebruik

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 const readTXT = require('./functies/readTXT');
 const multiReis = require('./functies/multiReis.js');
-const genereerHTMLResulataat = require('./functies/genereerHTMLResulataat.js');
-const openHTML = require('./functies/openHTML.js');
 const formatteerReis = require('./functies/formatteerReis.js');
 const writeTXT = require('./functies/writeTXT.js');
 
 (async () => {
     const reis = await multiReis(await readTXT("route"));
-
-    const reisHTML = genereerHTMLResulataat(reis);
-    openHTML(reisHTML, 5000);
-
     const reisScriptNederlands = formatteerReis(reis);
-    console.log(reisScriptNederlands);
 
+    console.log(reisScriptNederlands);
     writeTXT(reisScriptNederlands, "reis");
-    writeTXT(reisHTML, "reishtml");
-    writeTXT(reis.gepasseerdestations, "gepasseerd");
-    writeTXT(reis.urls, "bewijs");
 })();
