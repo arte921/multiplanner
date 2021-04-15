ad2f399a52611730432c9391b16508ff034bb4bb
Ard-Jan van Etten <ardjanve@gmail.com>
2021-04-15 21:24:13 +0200

    style pagina

--- functies/genereerHTMLResulataat.js
+++ functies/genereerHTMLResulataat.js
@@ -26,15 +26,23 @@ module.exports = (reis) => {
     `).join("");
 
     return `
+<!DOCTYPE html>
+<html lang="en">
 
 <html>
     <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
         <title>Reis van ${reis.reis[0].vertrekstationnaam} naar ${reis.reis[reis.reis.length - 1].aankomststationnaam}</title>
+        <link rel="stylesheet" href="bootstrap-neon-glow.css">
+        
         <link rel="stylesheet" href="resultaat.css">
         <script>
         // Initialize and add the map
         function initMap() {
-            const map = new google.maps.Map(document.getElementById("map"), {
+            const map = new google.maps.Map(document.getElementById("google-map-reis"), {
                 zoom: 8,
                 center: { lat: 52.2587, lng: 5.6054 },
                 mapTypeId: "terrain",
@@ -52,42 +60,61 @@ module.exports = (reis) => {
         </script>
     </head>
     <body>
-        <div id="map"></div>
-        <br>
-        <table>
-            <tr><th>Prijs</th><td>&euro;${reis.prijs / 100}</td></tr>
-            <tr><th>Vertrekdatum</th><td>${formatteerDatum(reis.reis[0].vertrektijd)}</td></tr>
-            <tr><th>Wachttijd</th><td>${formateerTijdsduurMinuten(reis.stationstijd)}</td></tr>
-            <tr><th>Rijtijd</th><td>${formateerTijdsduurMinuten(reis.treintijd)}</td></tr>
-            <tr><th>Totale reistijd</th><td>${formateerTijdsduurMinuten(reis.reistijd)}</td></tr>
-            <tr><th>Hemelsbrede afstand</th><td>${Math.round(reis.hemelsbredeafstand)} kilometer</td></tr>
-            <tr><th>Afgelegde afstand</th><td>${Math.round(reis.afstand)} kilometer</td></tr>
-        </table>
-        <br>
-        <table>
-            <th>Overstaptijd</th>
-            <th>Vertrekstation</th>
-            <th>Vertrekspoor</th>
-            <th>Vertrektijd</th>
-            <th>Product</th>
-            <th>Richting</th>
-            <th>Ritduur</th>
-            <th>Aankomsttijd</th>
-            <th>Aankomststation</th>
-            <th>Aankomstspoor</th>
-            <th>Uitstapzijde</th>
-            ${reisTabel}
-        </table>
-        <br>
-        <b>Bewijs links</b>
-        <ul>
-            ${reis.urls.map((url, index) => `<li><a href=${url} target="_blank">Bewijslink ${index + 1}</a></li>`).join("")}
-        </ul>
-        <br>
-        <b>Gepasseerde stations</b>
-        <ul><li>
-            ${reis.gepasseerdestations.join("</li><li>")}
-        </li></ul>
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col">
+                    <h3>Reisdetails</h3>
+                    <div class="table-responsive">
+                        <table class="table table-hover">
+                            <tr><th scope="row">Prijs</th><td>&euro;${reis.prijs / 100}</td></tr>
+                            <tr><th scope="row">Vertrekdatum</th><td>${formatteerDatum(reis.reis[0].vertrektijd)}</td></tr>
+                            <tr><th scope="row">Wachttijd</th><td>${formateerTijdsduurMinuten(reis.stationstijd)}</td></tr>
+                            <tr><th scope="row">Rijtijd</th><td>${formateerTijdsduurMinuten(reis.treintijd)}</td></tr>
+                            <tr><th scope="row">Totale reistijd</th><td>${formateerTijdsduurMinuten(reis.reistijd)}</td></tr>
+                            <tr><th scope="row">Hemelsbrede afstand</th><td>${Math.round(reis.hemelsbredeafstand)} kilometer</td></tr>
+                            <tr><th scope="row">Afgelegde afstand</th><td>${Math.round(reis.afstand)} kilometer</td></tr>
+                        </table>
+                    </div>
+                </div>
+
+                <div class="map-container col" id="google-map-reis"></div>
+
+                <div class="col">
+                    <h3>Reisplan</h3>
+                    <table class="table table-hover">
+                        <th scope="col">Overstaptijd</th>
+                        <th scope="col">Vertrekstation</th>
+                        <th scope="col">Vertrekspoor</th>
+                        <th scope="col">Vertrektijd</th>
+                        <th scope="col">Product</th>
+                        <th scope="col">Richting</th>
+                        <th scope="col">Ritduur</th>
+                        <th scope="col">Aankomsttijd</th>
+                        <th scope="col">Aankomststation</th>
+                        <th scope="col">Aankomstspoor</th>
+                        <th scope="col">Uitstapzijde</th>
+                        ${reisTabel}
+                    </table>
+                </div>
+           
+                <div class="col">
+                    <h3>Bewijs links</h3>
+                    <ul class="list-group">
+                        ${reis.urls.map((url, index) => `<li class="list-group-item"><a class="bewijslink" href=${url} target="_blank">Bewijslink ${index + 1}</a></li>`).join("")}
+                    </ul>
+                </div>
+                <div class="col">
+                    <h3>Gepasseerde stations</h3>
+                    <ul class="list-group"><li class="list-group-item">
+                        ${reis.gepasseerdestations.join(`</li><li class="list-group-item">`)}
+                    </li></ul>
+                </div>
+            </div>
+        </div>
+
         <script async
             src="https://maps.googleapis.com/maps/api/js?key=${config.google_maps_api_key}&callback=initMap">
         </script>
